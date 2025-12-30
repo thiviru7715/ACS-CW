55d4d3525976bafedab7451cb0b7abb2f7bc0be5
Thiviru Igalawithana <thivirudamsith7715@gmail.com>
2025-12-30 19:43:59 +0530

    change browserRouter to hashRouter

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,21 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import PropertyPage from "./pages/PropertyPage";
 import Layout from "./components/Layout";
 import FavouritesPanel from "./components/FavouritePanel";
 import { FavouritesProvider } from "./context/FavouritesContext";
 
+
 function App() {
   return (
     <FavouritesProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<SearchPage />} />
             <Route path="property/:id" element={<PropertyPage />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       </FavouritesProvider>
   );
 }
